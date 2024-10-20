f9fea5601de267bc2eddb5a884bb14ffd9fedb8f
frost-racc <mimi.rostek@gmail.com>
2024-10-20 16:11:29 +0200

    changed welcome screen, added assets, added lualine

--- init.lua
+++ init.lua
@@ -1,8 +1,10 @@
-require("config.lazy")
-require("config.telescope")
-
 vim.opt.number = true
 vim.opt.mouse = 'a'
 vim.opt.autoindent = true
 vim.opt.autoread = true
 vim.cmd("colorscheme retrobox")
+
+--plugins
+require("config.lazy")
+require("config.telescope")
+require("config.lualine")
