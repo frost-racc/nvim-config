cfb21921867ce4c0a7dcc4585138b89b8e2a781f
frost-racc <mimi.rostek@gmail.com>
2025-03-12 02:11:56 +0100

    kanagawa colorscheme, lsp formatting setup, init.lua options + keymaps

--- init.lua
+++ init.lua
@@ -1,20 +1,61 @@
---plugins
+--settings
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+vim.opt.number = true
+vim.opt.mouse = 'a'
+vim.opt.expandtab = true
+vim.opt.autoindent = true
+vim.opt.smartindent = false
+vim.opt.cindent = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.autoread = true
+vim.opt.clipboard = 'unnamedplus'
+vim.opt.breakindent = true
+vim.opt.signcolumn = 'yes'
+vim.opt.updatetime = 250
+vim.opt.timeoutlen = 300
+vim.opt.inccommand = 'split'
+vim.g.have_nerd_font = true
+vim.opt.relativenumber = true
+vim.opt.undofile = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+-- Sets how neovim will display certain whitespace characters in the editor.
+--  See `:help 'list'`
+--  and `:help 'listchars'`
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+vim.opt.cursorline = true
+
+
+
+
+--plugin configs
 require("config.lazy")
 require("config.telescope")
 require("config.lualine")
 require("config.neo-tree")
 
---settings
-vim.opt.number = true
-vim.opt.mouse = 'a'
-vim.opt.autoindent = true
-vim.opt.autoread = true
+
 
 --colorscheme
-vim.cmd("colorscheme nightfox")
+vim.cmd("colorscheme kanagawa")
 
 --mappings
 vim.keymap.set('n', '<Leader>hm', ':Alpha<CR>')
-vim.keymap.set({'n', 'i', 'v'}, '<Leader>[', ':Neotree left toggle<CR>')
-vim.keymap.set({'n', 'i', 'v'}, '<Leader>]', ':Neotree float toggle<CR>')
+vim.keymap.set('n', '<Leader>[', ':Neotree left toggle<CR>')
+vim.keymap.set('n', '<Leader>]', ':Neotree float toggle<CR>')
 vim.keymap.set('n', '<Leader>zm', ':ZenMode<CR>')
+
+-- Clear highlights on search when pressing <Esc> in normal mode
+--  See `:help hlsearch`
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+-- Diagnostic keymaps
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -1,45 +1,46 @@
 return {
-"goolord/alpha-nvim",
-     config = function ()
-         local alpha = require'alpha'
-         local dashboard = require'alpha.themes.dashboard'
-         dashboard.section.header.val = {
+	"goolord/alpha-nvim",
+	config = function()
+		local alpha = require 'alpha'
+		local dashboard = require 'alpha.themes.dashboard'
+		dashboard.section.header.val = {
 
-[[█▀ █▀▄ ▄▀▄ ▄▀▀ ▀█▀ ▀▄▀     █▄ █ ██▀ ▄▀▄ █ █ █ █▄ ▄█]],
-[[█▀ █▀▄ ▀▄▀ ▄██  █   █      █ ▀█ █▄▄ ▀▄▀ ▀▄▀ █ █ ▀ █]],
-[[                                                   ]],
-[[               ███▄▄▄   ▄▄▄   ▄▄▄███               ]],
-[[               █    ▀▀▀▀   ▀▀▀▀    █               ]],
-[[                █                 █                ]],
-[[                ▄█  ▄▄▄     ▄▄▄  █▄                ]],
-[[               ▄█  █▀▀██   ██▀▀█  █▄               ]],
-[[             ▀█   ██ ▄█     █▄ ██   █▀             ]],
-[[               ▀▄ ███▀  ███  ▀███ ▄▀               ]],
-[[               ▀█      ▄ █ ▄      █▀               ]],
-[[                 ▀▀█▄   ▀▀▀   ▄█▀▀                 ]],
-[[                     ▀▀▄▄▄▄▄▀▀                     ]],}
+			[[█▀ █▀▄ ▄▀▄ ▄▀▀ ▀█▀ ▀▄▀     █▄ █ ██▀ ▄▀▄ █ █ █ █▄ ▄█]],
+			[[█▀ █▀▄ ▀▄▀ ▄██  █   █      █ ▀█ █▄▄ ▀▄▀ ▀▄▀ █ █ ▀ █]],
+			[[                                                   ]],
+			[[               ███▄▄▄   ▄▄▄   ▄▄▄███               ]],
+			[[               █    ▀▀▀▀   ▀▀▀▀    █               ]],
+			[[                █                 █                ]],
+			[[                ▄█  ▄▄▄     ▄▄▄  █▄                ]],
+			[[               ▄█  █▀▀██   ██▀▀█  █▄               ]],
+			[[             ▀█   ██ ▄█     █▄ ██   █▀             ]],
+			[[               ▀▄ ███▀  ███  ▀███ ▄▀               ]],
+			[[               ▀█      ▄ █ ▄      █▀               ]],
+			[[                 ▀▀█▄   ▀▀▀   ▄█▀▀                 ]],
+			[[                     ▀▀▄▄▄▄▄▀▀                     ]], }
 
 
 
-	dashboard.section.buttons.val = {
-  	dashboard.button("f", " [~] Find file", ":Telescope find_files <CR>"),
-   	dashboard.button("e", " [+] New file", ":ene <BAR> startinsert <CR>"),
-   	dashboard.button("r", "󱋡 [>] Recently used files", ":Telescope oldfiles <CR>"),
-  	dashboard.button("c", " [o] Configuration", ":cd C:/Users//AppData/Local/nvim<CR> :e init.lua<CR>"),
-   	dashboard.button("q", "󰈆 [x] Quit Neovim", ":qa<CR>"),
+		dashboard.section.buttons.val = {
+			dashboard.button("f", " [~] Find file", ":Telescope find_files <CR>"),
+			dashboard.button("e", " [+] New file", ":ene <BAR> startinsert <CR>"),
+			dashboard.button("r", "󱋡 [>] Recently used files", ":Telescope oldfiles <CR>"),
+			dashboard.button("c", " [o] Configuration",
+				":cd C:/Users/mimir/AppData/Local/nvim<CR> :e init.lua<CR>"),
+			dashboard.button("q", "󰈆 [x] Quit Neovim", ":qa<CR>"),
+		}
+
+
+		dashboard.section.footer.val = {
+			[[                                                      ]],
+			[["NeoVim is like peanut butter...  you have to like it"]],
+			[[                                     --Abraham Lincoln]] }
+		dashboard.config.opts.noautocmd = true
+
+		vim.cmd [[autocmd User AlphaReady echo 'ready']]
+
+		alpha.setup(dashboard.config)
+	end,
+
+
 }
-
-
-        dashboard.section.footer.val = {
-		[[                                                      ]],
-		[["NeoVim is like peanut butter...  you have to like it"]],
-		[[                                     --Abraham Lincoln]]}
-        dashboard.config.opts.noautocmd = true
-         
-	    vim.cmd[[autocmd User AlphaReady echo 'ready']]
-
-         alpha.setup(dashboard.config)
-     end,
-
-	
- }
