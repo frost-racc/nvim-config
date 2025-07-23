728ad3fe709942127750be5404212a3deba561a8
frost-racc <mimi.rostek@gmail.com>
2025-07-23 18:40:15 +0200

    small bugs - working on nvim 11.2+ !!

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,28 +2,30 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function()
-    	local treesitter = require('nvim-treesitter.configs')
+        local treesitter = require('nvim-treesitter.configs')
 
-    	treesitter.setup({
-		highlight = {
-			enable = true,
-		},
-		indent = {
-			enable = true,
-		},
-		
-		ensure_installed = {
-		"json",
-		"javascript",
-		"html",
-		"css",
-		"markdown",
-		"bash",
-		"lua",
-		"vim",
-		"c",
-		"python",
-	},
-    })
+        treesitter.setup({
+            highlight = {
+                enable = true,
+            },
+            indent = {
+                enable = true,
+            },
+
+            ensure_installed = {
+                "json",
+                "javascript",
+                "html",
+                "css",
+                "markdown",
+                "bash",
+                "lua",
+                "vim",
+                "c",
+                "python",
+            },
+            auto_install = true,
+
+        })
     end,
 }
